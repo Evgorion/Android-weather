959e092799ad768f4540007cc024f9f55d7707d4
Evgorion <y071962@gmail.com>
2015-11-29 16:40:54 +0200

    Add new JavaDoc information

--- Android-weather/app/src/main/java/com/dagaz/evgeny/android_weather/MainActivity.java
+++ Android-weather/app/src/main/java/com/dagaz/evgeny/android_weather/MainActivity.java
@@ -3,6 +3,15 @@ package com.dagaz.evgeny.android_weather;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * Это моя первая программа для поставки метео инофрмации.
+ *
+ * С помощью данной программы вы сможете получить огромнео количество разнообразной метео информации.
+ *
+ * @author Evgeny
+ * @version 1.0.0 Build 3 29.11.2015
+ *
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
